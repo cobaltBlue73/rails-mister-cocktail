ae06ac7fe7c9d10f2b2c595941268bd6d9abdd27
MuhammadZaid Amir <md.zaid73@gmail.com>
2020-09-11 16:13:35 +0800

    added active storage

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -43,6 +43,6 @@ class CocktailsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
